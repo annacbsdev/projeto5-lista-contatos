1449d817ddd6760a7dc6dfc4cfb186e48931365e
Anna Claudia <annaclaudiadev24@gmail.com>
2024-05-01 12:08:43 -0300

    Projeto lista de contatos

---
+++ src/styles/index.ts
@@ -0,0 +1,53 @@
+import styled, { createGlobalStyle } from 'styled-components'
+import _var from './var'
+
+const EstiloGlobal = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    list-style: none;
+  }
+  body{
+    background-color: ${_var.verde}
+  }
+
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+`
+
+export const Titulo = styled.h1`
+  font-weight: normal;
+  font-size: 56px;
+  text-align: center;
+  margin: 56px 0;
+`
+
+export const Content = styled.div`
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 32px;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 24px;
+`
+
+export const Botao = styled.button`
+  border-radius: 32px;
+  border: none;
+  color: white;
+  padding: 16px;
+  background-color: ${_var.verde};
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 600;
+  margin: 24px auto 0;
+`
+
+export default EstiloGlobal
---
+++ src/styles/var.ts
@@ -0,0 +1,5 @@
+export default {
+  verde: '#4AC0C5',
+  vermelho: '#FF5757',
+  cinza: '#737373'
+}
